fc58d40d03e5cac83a97d09c04b5e85adebb5a9c
Jessica-Asamoah <61128404+Jessica-Asamoah@users.noreply.github.com>
2023-03-21 18:35:30 -0700

    A program that prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry point
- *
+ * Description: It prints the word _putchar, followed by a new line
  * Return: Always 0 (Success)
  * 
  */
 int main(void)
 {
-	printf("%d\n",_putchar);
+	printf("%s\n","_putchar")
 	return (0);
 }
