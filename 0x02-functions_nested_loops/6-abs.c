a05a60d2ed23e4453535897c5d2034bb4303688c
Jessica-Asamoah <61128404+Jessica-Asamoah@users.noreply.github.com>
2023-03-21 20:53:59 -0700

    Absolute function

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _abs - entry point
+ * @r: an input parameter
+ * Description: computes the absolute value of an integer
+ * Return: return absolute integer value
+ */
+int _abs(int r)
+{
+	
+	if (r >= 0)
+	{
+		return (r);
+	}
+	else
+	
+		return (r * -1);
+}
