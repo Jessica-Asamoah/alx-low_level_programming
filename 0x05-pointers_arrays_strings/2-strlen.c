9f725b842492ffd4b46cd560ac831964874c63f9
Jessica-Asamoah <61128404+Jessica-Asamoah@users.noreply.github.com>
2023-03-27 18:54:25 -0700

    returns length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+	sizeof(&s);
+	len = 1;
+
+	return (len);
+}
