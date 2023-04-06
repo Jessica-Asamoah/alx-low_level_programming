81a2ebf916acde1b2c5fd5964d743bfcc2c16419
Jessica-Asamoah <61128404+Jessica-Asamoah@users.noreply.github.com>
2023-04-05 18:47:57 -0700

    Printing a string followed by a new line

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - Entry point
+ * @s: input character
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	while (*s)
+	{
+		_putchar(*S);
+	}
+	_putchar('\n');
+}
