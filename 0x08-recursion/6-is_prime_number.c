db6d80b4adae6da0416db8771f0479b214afd1ba
Jessica-Asamoah <61128404+Jessica-Asamoah@users.noreply.github.com>
2023-04-05 20:21:46 -0700

    checks if a number is a prime number or otherwise

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+int divide(int i, int mod);
+
+/**
+ * is_prime_number - Entry point
+ * @n: input integer
+ * Return returns 1 if prime and 0 otherwise
+ */
+
+int is_prime_number(int n)
+{
+	int mod = 2;
+
+	if (n <= 1)
+		return (0);
+	if (n <= 3)
+		return (1);
+
+	return (divide(n, mod));
+}
+
+/**
+ * divide - entry point
+ * @i: input integer
+ * @mod: input integer
+ * return: return
+ */
+
+int divide(int i, int mod)
+{
+	if (i % mod == 0)
+		return (0);
+
+	if (mod == i / 2)
+		return (1);
+
+	return (divide(i, mod + 1));
+}
