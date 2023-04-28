7b65627b9afd67315dce859e06bbfbbdcbb23b05
Jessica-Asamoah <jessicaasamoah903@gmail.com>
2023-04-28 09:47:39 +0000

    Splits a string into words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,51 +1,111 @@
-#include "main.h"
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * strtow - A function that splits a string into words
- * @str: An input pointer of the string to split
+ * count_words - counts words in a string
+ * @str: the string
  *
- * Return: A pointer to concatenate strings or NULL if str is NULL
+ * Return: number of words
+ */
+int count_words(char *str)
+{
+	int spaces, words, i;
+
+	i = 0;
+	spaces = 1;
+	words = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == ' ')
+		{
+			spaces++;
+		}
+		else
+		{
+			if (spaces > 0)
+			{
+				words++;
+				spaces = 0;
+			}
+		}
+		i++;
+	}
+	return (words);
+}
+
+/**
+ * next_word - gets the index of hte next word
+ * @str: the string
+ * @start: the start index
+ *
+ * Return: index of the word, else -1
+ */
+int next_word(char *str, int start)
+{
+	int spaces = 1;
+
+	while (str[start] != '\0')
+	{
+		if (str[start] == ' ')
+		{
+			spaces++;
+		}
+		else
+		{
+			if (spaces > 0)
+				return (start);
+		}
+		start++;
+	}
+	return (-1);
+}
+
+/**
+ * word_len - gets the length of a word
+ * @str: the string
+ * @start: start index
+ *
+ * Return: length of the word
+ */
+int word_len(char *str, int start)
+{
+	int len = 0;
+
+	while (str[start + len] != ' ' && str[start + len] != '\0')
+		len++;
+	return (len);
+}
+
+/**
+ * strtow - converts string to array of words
+ * @str: the string
+ *
+ * Return: array of strings
  */
 char **strtow(char *str)
 {
-	char **array;
-	int i = 0, j, m, k = 0, len = 0, count = 0;
+	char **arr = NULL;
+	int k = 0;
+	int words, i, j, len;
 
-	if (str == NULL || *str == '\0')
+	words = count_words(str);
+
+	arr = malloc(sizeof(char *) * (words + 1));
+	if (arr == NULL)
 		return (NULL);
-	for (; str[i]; i++)
+	i = next_word(str, 0);
+	while (i != -1)
 	{
-		if ((str[i] != ' ' || *str != '\t') &&
-				((str[i + 1] == ' ' || str[i + 1] == '\t') || str[i + 1] == '\n'))
-			count++;
+		len = word_len(str, i);
+		arr[k] = malloc(sizeof(char) * (len + 1));
+		if (arr[k] == NULL)
+			return (NULL);
+		for (j = 0; j < len; j++)
+			arr[k][j] = str[i + j];
+		arr[k][j] = '\0';
+		k++;
+		i = next_word(str, i + j);
 	}
-	if (count == 0)
-		return (NULL);
-	array = malloc(sizeof(char *) * (count + 1));
-	if (array == NULL)
-		return (NULL);
-	for (i = 0; str[i] != '\0' && k < count; i++)
-	{
-		if (str[i] != ' ' || str[i] != '\t')
-		{
-			len = 0;
-			j = i;
-			while ((str[j] != ' ' || str[j] != '\t') && str[j] != '\0')
-				j++, len++;
-			array[k] = malloc((len + 1) * sizeof(char));
-			if (array[k] == NULL)
-			{
-				for (k = k - 1; k >= 0; k++)
-					free(array[k]);
-				free(array);
-				return (NULL);
-			}
-			for (m = 0; m < len; m++, i++)
-				array[k][m] = str[i];
-			array[k++][m] = '\0';
-		}
-	}
-	array[k] = NULL;
-	return (array);
+	arr[k] = NULL;
+	return (arr);
 }
