0aaed25286fe01fc0b7fdfecd2138cf907266976
Jessica-Asamoah <jessicaasamoah903@gmail.com>
2023-06-08 14:21:11 +0000

    Recursion: checking if a string is a palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,56 @@
+#include "main.h"
+#include <stdio.h>
+
+int _strlen_recursion(char *s);
+int helper_palindrome(char *s, int len);
+
+/**
+ * _strlen_recursion - Function to obtain the length of s
+ * @s: string that calculate length
+ * Return: length of s
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (!*s)
+	{
+		return (0);
+	}
+	return (_strlen_recursion(s + 1) + 1);
+}
+
+/**
+ * is_palindrome - Function that checks if the string is a palindrome
+ * @s: Input string
+ *
+ * Return: 1(palindrome) 0(otherwise)
+ */
+
+int is_palindrome(char *s)
+{
+	int len;
+
+	len = _strlen_recursion(s);
+	if (len <= 1)
+		return (1);
+	return (helper_palindrome(s, len));
+}
+
+/**
+ * helper_palindrome - Function for a reversed string
+ * @s: Input string
+ * @len: the length of the string s
+ * Return: reverse string
+ */
+
+int helper_palindrome(char *s, int len)
+{
+	if (len <= 1)
+		return (1);
+	else if (*s == *(s + len - 1))
+	{
+		return (helper_palindrome(s + 1, len - 2));
+	}
+	else
+		return (0);
+}
