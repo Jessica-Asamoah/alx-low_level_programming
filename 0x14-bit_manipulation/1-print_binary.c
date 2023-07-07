31ed91c7e5e01381d85409902e207a1b524adc92
Jessica-Asamoah <jessicaasamoah903@gmail.com>
2023-07-07 02:11:21 +0000

    function that prints the binary representation of a number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * _mod2 - returns the modulo 2 of a number
+ * @n: Number
+ *
+ * Return: modulo 2
+ */
+
+unsigned long int _mod2(unsigned long int n)
+{
+	return (n - ((n >> 1) * 2));
+}
+
+/**
+ * _print_binary - Function that prints the binary representation
+ * of a number
+ * @n: Number
+ *
+ * Return: void
+ */
+
+void _print_binary(unsigned long int n)
+{
+	if (n == 0)
+		return;
+	_print_binary(n >> 1);
+	_putchar('0' + _mod2(n));
+}
+
+/**
+ * print_binary - Function that prints the binary representation
+ * of a number
+ * @n: Number
+ *
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	else
+	{
+		_print_binary(n);
+	}
+}
